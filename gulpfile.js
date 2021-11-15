22ee63dd727666f270712b9613b71801cc33e4c2
dhougodamerina <dhougoda.merina@wensolutions.com>
2021-11-15 12:31:12 +0545

    tasks

---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+// list dependences
+const gulp = require('gulp');
+const { src, dest, watch, series } = require('gulp');
+const htmlmin = require('gulp-htmlmin');
+const sass = require('gulp-sass')(require('sass'));
+const prefix = require('gulp-autoprefixer');
+const minify = require('gulp-clean-css');
+const imagewebp = require('gulp-webp');
+const optimage = require('gulp-image');
+
+
+//create functions 
+//html
+function minhtml() {
+    return src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true}))
+    .pipe(dest('dist'));
+}
+
+//scss
+function compilescss () {
+    return src('src/scss/*.scss')
+    .pipe(sass())
+    .pipe(prefix())
+    .pipe(minify())
+    .pipe(dest('dist/scss'))
+}
+function compilecss () {
+    return src('src/bootstrap/*.css'), src('src/icons/*.css')
+    .pipe(prefix())
+    .pipe(minify())
+    .pipe(dest('dist/css'))
+}
+function compileboot () {
+    return src('src/bootstrap/*.css')
+    .pipe(prefix())
+    .pipe(minify())
+    .pipe(dest('dist/bootstrap'))
+}
+
+//Images
+function optimizeimage() {
+    return src('src/images/*.{jpg,png}')
+    .pipe(optimage())
+    .pipe(dest('dist/images'))
+}
+
+function webpImage() {
+    return src('src/images/*.{jpg,png}')
+    .pipe(imagewebp())
+    .pipe(dest('dist/images'))
+}
+//create watchtask
+function watchtask() {
+    watch('src/scss/*.scss', compilescss);
+    watch('dist/images/*.{jpg, png}', webpImage);
+}
+
+//default gulp
+exports.default = series(
+    minhtml,
+    compilescss,
+    compilecss,
+    compileboot,
+    optimizeimage,
+    webpImage,
+    watchtask
+);
